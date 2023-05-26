ddfebdacff4c4c9b17380550763dc89e6067564a
riley-seoyoon <sypark02178@gmail.com>
2023-05-26 14:11:50 +0900

    Add pychopper to workflow

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# apa-covid
+# APA
+
+## Project Overview
+
+Alternative Polyadenylation (APA) analysis for long-read Nanopore COVID-19 data
+
+## Source Data
+A multi-omics investigation of the composition and function of extracellular vesicles along the temporal trajectory of COVID-19 ([link](https://www.nature.com/articles/s42255-021-00425-4))
+
+GEO accession: [GSE174668](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE174668)
+
+## Analysis Workflow
+
+### 1. Quality Control
+[pychopper](https://github.com/epi2me-labs/pychopper)
+
+### 2. Alignment
+
+[minimap2](https://github.com/lh3/minimap2)
+
+### 3. Identification of APA events
+
+[LorTIA](https://github.com/zsolt-balazs/LoRTIA)
+
+### 4. Differential analysis of APA
+
+In-house mthod based on [DaPars2](https://github.com/3UTR/DaPars2)
+
+Dependencies listed in requirements.txt
--- analysis/run_apa.py
+++ analysis/run_apa.py
@@ -12,35 +12,69 @@ wandb.init(
     # track hyperparameters and run metadata
     config={
         "model": "lortia",
-        "dataset": "covid-hepg2",
+        "dataset": "covid-nanopore",
+        "working_dir": "/mnt/e/Documents/Remote/lab/APA",
+        "sam_path": "/mnt/e/Documents/Remote/lab/APA/aln",
+        "fa_file": "/mnt/e/Documents/APA/Homo_sapiens.GRCh38.cdna.all.fa",
+        "gtf_file": "/mnt/e/Documents/APA/Homo_sapiens.GRCh38.109.gtf",
+        "fq_files": "/mnt/e/Documents/APA/fastq/*.fastq",
+        "lortia_path": "/mnt/e/Documents/Remote/lab/APA/LoRTIA/",
+        "lortia_config": "-5 TGCCATTAGGCCGGG --five_score 16 --check_in_soft 15 -3 AAAAAAAAAAAAAAA --three_score 16 -s poisson -f True",
+        "pychopper_config": "-U -y",
+        "minimap2_config": "-ax splice",
     },
 )
 
-wd = "/mnt/e/Documents/Remote/lab/APA"
-# bam_file_path = "/Users/seoyoonpark/Library/CloudStorage/GoogleDrive-sypark217@eunbi.co.uk/Other computers/My computer/APA/A549/*.bam"
-sam_file_path = f"{wd}/aln/*.sam"
-transcriptome_file = "/mnt/e/Documents/APA/Homo_sapiens.GRCh38.cdna.all.fa"
-gtf_file = "/mnt/e/Documents/APA/Homo_sapiens.GRCh38.109.gtf"
-fq_files = glob.glob("/mnt/e/Documents/APA/fastq/*.fastq")
-lortia_path = "/mnt/e/Documents/Remote/lab/APA/LoRTIA/"
+# Set variables from wandb configuration
+wd = wandb.config["working_dir"]
+sam_path = wandb.config["sam_path"]
+fa_file = wandb.config["fa_file"]
+gtf_file = wandb.config["gtf_file"]
+fq_files = glob.glob(wandb.config["fq_files"])
+lortia_path = wandb.config["lortia_path"]
+chop_files = glob.glob(f"{wd}/chop/*_full.fq")
 
 
-# Run Minimap2
-def run_minimap2(fq_files):
-    subprocess.run(f"mkdir -p '{wd}/aln/'", shell=True)
+def run_pychopper(fq_files):
+    subprocess.run(f"mkdir -p '{wd}/chop/'", shell=True)
     for fq in fq_files:
+        # Extract sample names from files
         sample = os.path.splitext(fq)[0].split("/")[-1]
-        sam = sample + ".sam"
-        # Run script on fq file
         subprocess.run(
-            f"minimap2 -ax splice {transcriptome_file} {fq} > {wd}/aln/{sam}",
+            f"pychopper -U -y -r {wd}/chop/{sample}_report.pdf -u {wd}/chop/{sample}_unclassified.fq \
+                -w {wd}/chop/{sample}_rescued.fq -S {wd}/chop/{sample}_stats.txt \
+                    {fq} {wd}/chop/{sample}_full.fq",
             shell=True,
         )
 
 
+# Run Minimap2
+def run_minimap2(fq_files):
+    # Make minimap2 output directory if it doesn't exist
+    subprocess.run(f"mkdir -p '{wd}/aln/'", shell=True)
+    for fq in fq_files:
+        # Extract sample names from files
+        sample = os.path.splitext(fq)[0].split("/")[-1]
+        sam = sample + ".sam"
+        if (
+            glob.glob(f"{sam_path}/{sample}.sam") == []
+        ):  # If sam file for sample doesn't exist
+            subprocess.run(
+                f"echo'Running minimap2' && minimap2 -ax splice {fa_file} {fq} > {wd}/aln/{sam} && \
+                    echo 'Running samtools flagstat' && \
+                    samtools flagstat {sam_path}/{sample}.sam > {sam_path}/{sample}.flagstat.txt",
+                shell=True,
+            )  # Run minimap2 and samtools flagstat
+        else:  # If sam file for sample already exists only run samtools flagstat
+            subprocess.run(
+                f"samtools flagstat {sam_path}/{sample}.sam > {sam_path}/{sample}.flagstat.txt",
+                shell=True,
+            )
+
+
 # Run LoRTIA
-def run_script_on_sams(sam_file_path):
-    sam_files = glob.glob(sam_file_path)
+def run_script_on_sams(sam_path):
+    sam_files = glob.glob(f"{sam_path}/*.sam")
     subprocess.run(f"mkdir -p '{wd}/lortia_results/'", shell=True)
     for sam in sam_files:
         # Run script on bam file
@@ -48,7 +82,7 @@ def run_script_on_sams(sam_file_path):
             f"{lortia_path}/LoRTIA -5 TGCCATTAGGCCGGG --five_score 16 \
                        --check_in_soft 15 -3 AAAAAAAAAAAAAAA --three_score 16 -s poisson -f True \
                        {sam} {wd}/lortia_results/ \
-                       {transcriptome_file}",
+                       {fa_file}",
             shell=True,
         )
 
@@ -57,15 +91,20 @@ class minimap_err(Exception):
     "Raised when minimap2 failed and there are no sam files"
     pass
 
+run_pychopper(fq_files)
 
-try:
-    run_minimap2(fq_files)
-    if glob.glob(sam_file_path) == []:
-        raise minimap_err
-except minimap_err:
-    subprocess.run(f"rmdir '{wd}/aln/'", shell=True)
-    print("failed to run minimap2")
+if glob.glob(f"{sam_path}/*.flagstat.txt") != []:  # If flagstat is already run
+    print("Running LorTIA on existiing sam files")
+    run_script_on_sams(sam_path)
 else:
-    run_script_on_sams(sam_file_path)
+    try:
+        run_minimap2(chop_files)
+    except minimap_err:  # if minimap2 failed
+        print("Failed to run minimap2")
+    else:
+        try:
+            run_script_on_sams(sam_path)
+        except:
+            print("Failed to run LoRTIA")
 
 wandb.finish()
